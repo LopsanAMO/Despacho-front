71c2bb8df42ab0901c9ceb0dd0000b67eaacafaf
LopsanAMO <lopsan.molina@gmail.com>
2018-04-03 18:35:21 -0500

    added some url

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1/',
+  baseURL: 'http://34.218.115.218/api/v1/',
 });
 
 // instance.defaults.headers.common['Authorization'] = 'something'
